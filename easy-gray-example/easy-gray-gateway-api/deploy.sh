4693d40fbae24b29bc3174fb68a3f7b707d3c5d2
liupenghao <liupenghao@huohua.cn>
2023-02-16 16:45:09 +0800

    feat: 更新配置，方便测试

--- easy-gray-example/easy-gray-gateway-api/deploy.sh
+++ easy-gray-example/easy-gray-gateway-api/deploy.sh
@@ -1,12 +1,11 @@
-BUILD_NUMBER=$1
 echo "<<====================== 1.1 构建docker镜像 ===================>>"
-docker build -t registry.cn-hangzhou.aliyuncs.com/ranmo/easy-gray-gateway-api:${BUILD_NUMBER} -f Dockerfile .
+docker build -t registry.cn-hangzhou.aliyuncs.com/ranmo/easy-gray-gateway-api:6 -f Dockerfile .
 
 echo "<<====================== 1.2 登录docker仓库===================>>"
 docker login --username=16601114926 --password=270698050qQ registry.cn-hangzhou.aliyuncs.com
 
 echo "<<====================== 1.3 docker镜像 ===================>>"
-docker push registry.cn-hangzhou.aliyuncs.com/ranmo/easy-gray-gateway-api:${BUILD_NUMBER}
+docker push registry.cn-hangzhou.aliyuncs.com/ranmo/easy-gray-gateway-api:6
 
 
 echo "<<====================== 2. k8s.yaml中的镜像版本号 ===================>>"
