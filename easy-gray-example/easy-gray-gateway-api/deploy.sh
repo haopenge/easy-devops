b690cc43dcdda3c68a93e189998e8f6deab18f3a
xiaoyuxxx <anhusky@163.com>
2023-03-21 10:56:35 +0800

    feat: 修改配置

--- easy-gray-example/easy-gray-gateway-api/deploy.sh
+++ easy-gray-example/easy-gray-gateway-api/deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 DEPLOY_DIR=$1
 ALI_DOCKER_PWD=$2
+BUILD_NUMBER=$3
 if test -z "$DEPLOY_DIR"
     then
     echo "未输入发布目录"
-    return 0
+    exit 0
 else
   cd $DEPLOY_DIR
   echo "输入发布目录: ${DEPLOY_DIR} "
@@ -15,17 +16,16 @@ echo "<<====================== 1.1 开始打包 ===================>>"
 mvn clean package  -Dmaven.test.skip=true
 
 echo "<<====================== 1.2 构建docker镜像 ===================>>"
-docker build -t registry.cn-hangzhou.aliyuncs.com/ranmo/easy-gray-gateway-api:13 -f Dockerfile .
+docker build -t registry.cn-hangzhou.aliyuncs.com/ranmo/easy-gray-gateway-api:${BUILD_NUMBER} -f Dockerfile .
 
 echo "<<====================== 1.3 登录docker仓库===================>>"
 docker login --username=16601114926 --password=${ALI_DOCKER_PWD} registry.cn-hangzhou.aliyuncs.com
 
-echo "<<====================== 1.4 docker镜像 ===================>>"
-docker push registry.cn-hangzhou.aliyuncs.com/ranmo/easy-gray-gateway-api:13
-
+#echo "<<====================== 1.4 docker镜像 ===================>>"
+#docker push registry.cn-hangzhou.aliyuncs.com/ranmo/easy-gray-gateway-api:13
 
 echo "<<====================== 2. k8s.yaml中的镜像版本号 ===================>>"
-#sed -i "s/build_number/${BUILD_NUMBER}/g" k8s.yaml
+sed -i "s/build_number/${BUILD_NUMBER}/g" deployment.yaml
 
 echo "<<====================== 3. 发布服务 ===================>>"
 #kubectl apply -f k8s.yaml
