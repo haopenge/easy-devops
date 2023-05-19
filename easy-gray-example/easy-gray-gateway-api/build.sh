88ecf97f46fc6cbcf42af98c593b2f5e9f12697c
liupenghao <anhusky@163.com>
2023-05-19 16:31:16 +0800

    feat: 修改构建脚本

--- easy-gray-example/easy-gray-gateway-api/build.sh
+++ easy-gray-example/easy-gray-gateway-api/build.sh
@@ -12,23 +12,20 @@ DEPLOY_DIR=$(dirname "$SCRIPT")
 cd "${DEPLOY_DIR}"
 
 echo "发布目录: ${DEPLOY_DIR} "
-
 echo "<<====================== 1.1 开始打包 ===================>>"
 mvn clean package  -Dmaven.test.skip=true -Dmaven.javadoc.skip=true -s /root/.m2/settings.xml
 
-echo "<<====================== 1.2 构建docker镜像 ===================>>"
-docker build -t registry.cn-hangzhou.aliyuncs.com/ranmo/easy-gray-gateway-api:5192541 -f Dockerfile .
-
-echo "<<====================== 1.3 登录docker仓库===================>>"
+echo "<<====================== 1.2 登录docker仓库===================>>"
 docker login --username=${ALI_DOCKER_USER} --password=${ALI_DOCKER_PWD} registry.cn-hangzhou.aliyuncs.com
 
+echo "<<====================== 1.3 构建docker镜像 ===================>>"
+docker build -t registry.cn-hangzhou.aliyuncs.com/ranmo/easy-gray-gateway-api:${BUILD_NUMBER} -f Dockerfile .
+
 echo "<<====================== 1.4 docker镜像 ===================>>"
-docker push registry.cn-hangzhou.aliyuncs.com/ranmo/easy-gray-gateway-api:5192541
+docker push registry.cn-hangzhou.aliyuncs.com/ranmo/easy-gray-gateway-api:${BUILD_NUMBER}
 
 echo "<<====================== 2. k8s.yaml中的镜像版本号 ===================>>"
-sed -i "" "s/build_number/${BUILD_NUMBER}/g" deployment.yaml
-sed -i "" "s/pod-env/${GRAY_ENV}/g" deployment.yaml
-sed -i "" "s/pod-env/${GRAY_ENV}/g" service.yaml
-
+sed -i "s/build_number/${BUILD_NUMBER}/g" deployment.yaml
+sed -i "s/pod-env/${GRAY_ENV}/g" deployment.yaml
 
 
