f3c7435f7358d346081d2405569e79a5b0e48980
liupenghao <anhusky@163.com>
2023-06-07 10:09:56 +0800

    feat: 兼容多参数、批量操作自动更新

--- easy-devops-admin-api/src/main/java/com/easy/api/config/AuditInterceptor.java
+++ easy-devops-admin-api/src/main/java/com/easy/api/config/AuditInterceptor.java
@@ -10,8 +10,12 @@ import org.apache.ibatis.plugin.Invocation;
 import org.apache.ibatis.plugin.Plugin;
 import org.apache.ibatis.plugin.Signature;
 
-import java.util.List;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Objects;
 import java.util.Properties;
+import java.util.Set;
 
 /**
  * 审计拦截器
@@ -29,21 +33,28 @@ public class AuditInterceptor implements Interceptor {
         MappedStatement ms = (MappedStatement) args[0];
         Object obj = args[1];
 
-        if(args.length > 2 || obj instanceof List){
-            return invocation.proceed();
+        // 多参数、批量操作处理
+        Set<Object> objects = new HashSet<>();
+        if (obj instanceof Map) {
+            Map<?, ?> map = (Map<?, ?>) obj;
+            map.forEach((loopKey, loopValue) -> {
+                if (loopValue instanceof Map) {
+                    Map<?, ?> innnerMap = (Map<?, ?>) loopValue;
+                    objects.addAll(innnerMap.values());
+                }else if (loopValue instanceof Collection) {
+                    Collection<?> innerCollection = (Collection<?>) loopValue;
+                    objects.addAll(innerCollection);
+                }else {
+                    objects.add(loopValue);
+                }
+            });
+        } else {
+            objects.add(obj);
         }
 
         SqlCommandType sqlCommandType = ms.getSqlCommandType();
-        switch (sqlCommandType) {
-            case INSERT:
-                CommonAuditInterceptor.handler(obj, true);
-                break;
-            case UPDATE:
-                CommonAuditInterceptor.handler(obj, false);
-                break;
-            default:
-                break;
-        }
+        boolean isCreated = Objects.equals(sqlCommandType,SqlCommandType.INSERT);
+        objects.forEach(loopObj -> CommonAuditInterceptor.handler(loopObj, isCreated));
         return invocation.proceed();
     }
 
